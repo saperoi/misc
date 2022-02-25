07f1dc7befc649dc1020001357f2ee9a25f84331
saperoi <uesugi.sapero@gmail.com>
2022-02-25 23:37:51 +0100

    Update sekse.py

--- python/sekse.py
+++ python/sekse.py
@@ -1,6 +1,5 @@
 class Sekse:
     class gender:
-        self = []
         class low:
             self = []
             masc = ""
@@ -25,10 +24,11 @@ class Sekse:
         fem = ""
         andro = ""
         xenic = ""
-    
-    def __init__(self, genl, orient, rom, genu = None):
+    name = ""
+    def __init__(self, name, genl, orient, rom, genu = None):
         if genu == None:
             genu = genl
+        self.name = name
         self.gender.self = [genl, genu]
         self.gender.low.self = genl
         self.gender.upp.self = genu
@@ -45,7 +45,43 @@ class Sekse:
 
     def __repr__(self):
         return f"Sekse(Gender = {self.gender.low.self} ~ {self.gender.upp.self}, Sexuality = {self.orientation.self}, Romantic Orientation = {self.romantic.self})"
-
-meirl = Sekse((0, 0, 0, 0), (100, 100, 100, 25), (100, 100, 100, 25),(100, 10, 100, 0))
-print(meirl.gender.self)
-
+    
+    def sexual(a: "Sekse", b: "Sekse"):
+        mascatt = False
+        fematt = False
+        andratt = False
+        xenatt = False
+        if (b.gender.low.masc - 12.5) <= a.orientation.masc <= (b.gender.upp.masc + 12.5):
+            mascatt = True
+        if (b.gender.low.fem - 12.5) <= a.orientation.fem <= (b.gender.upp.fem + 12.5):
+            fematt = True
+        if (b.gender.low.andro - 12.5) <= a.orientation.andro <= (b.gender.upp.andro + 12.5):
+            andratt = True
+        if (b.gender.low.xenic - 12.5) <= a.orientation.xenic <= (b.gender.upp.xenic + 12.5):
+            xenatt = True
+        if mascatt == True and fematt == True and andratt == True and xenatt == True:
+            print(a.name + " is sexually attracted to " + b.name)
+            return True
+        else:
+            print(a.name + " is NOT sexually attracted to " + b.name)
+            return False
+    
+    def romance(a: "Sekse", b: "Sekse"):
+        mascatt = False
+        fematt = False
+        andratt = False
+        xenatt = False
+        if (b.gender.low.masc - 12.5) <= a.romantic.masc <= (b.gender.upp.masc + 12.5):
+            mascatt = True
+        if (b.gender.low.fem - 12.5) <= a.romantic.fem <= (b.gender.upp.fem + 12.5):
+            fematt = True
+        if (b.gender.low.andro - 12.5) <= a.romantic.andro <= (b.gender.upp.andro + 12.5):
+            andratt = True
+        if (b.gender.low.xenic - 12.5) <= a.romantic.xenic <= (b.gender.upp.xenic + 12.5):
+            xenatt = True
+        if mascatt == True and fematt == True and andratt == True and xenatt == True:
+            print(a.name + " is romantically attracted to " + b.name)
+            return True
+        else:
+            print(a.name + " is NOT romantically attracted to " + b.name)
+            return False
