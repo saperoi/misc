d20e4c969942c3912ed835a5c7ba1bbb456d17df
saperoi <uesugi.sapero@gmail.com>
2022-03-31 19:08:15 +0200

    math

--- python/math/chebyshev.py
+++ python/math/chebyshev.py
@@ -1,56 +1,76 @@
 from primegen import *
 import math
+import time
 
 def chebyshev_1(x):
-  primes = primegen(x)
-  summed = 0
-  for k in range(len(primes)):
-    summed += math.log(primes[k], math.e)
-  return summed
-
-def chebyshev_2(x):
-  primes = primegen(x)
-  summed = 0
-  for m in range(2, x+1):
-    if m in primes:
-      summed += math.floor(math.log(x, m)) * math.log(m, math.e)
-  return summed
-
-def epsi(x):
-  return math.exp(chebyshev_2(x))
+    primes = primegen(x)
+    summed = 0
+    for k in range(len(primes)):
+        summed += math.log(primes[k], math.e)
+    return summed
 
 def vonmangoldt(n):
-  m, k = pk(n)
-  if (m, k) == (0, 0):
-    return 0
-  else:
-    return math.log(m, math.e)
+    m, k = pk(n)
+    if (m, k) != (0, 0) and k >= 1:
+        p = math.log(m, math.e)
+    else:
+        p = float(0)
+    return p
 
 def lcm(x, y):
-  #not my code, math.lcm didnt exist????
-    if x > y:  
-        greater = x  
-    else:  
-        greater = y  
-    while(True):  
-        if((greater % x == 0) and (greater % y == 0)):  
-            lcm = greater  
-            break  
-        greater += 1  
-    return lcm
-  
+    #not my code, math.lcm didnt exist????
+        if x > y:    
+                greater = x    
+        else:    
+                greater = y    
+        while(True):    
+                if((greater % x == 0) and (greater % y == 0)):    
+                        lcm = greater    
+                        break    
+                greater += 1    
+        return lcm
+    
 def LCM_rep(x):
-  nums = []
-  for i in range(x):
-    nums.append(i+1)
-  if len(nums) == 1: return nums[0]
-  else:
-    c = lcm(nums[0], nums[1])
-    if len(nums) >= 2:
-      for i in range(2, len(nums)):
-        c = lcm(c, nums[i])
-  
-  return c
+    nums = []
+    for i in range(x):
+        nums.append(i+1)
+    if len(nums) == 1: return nums[0]
+    else:
+        c = lcm(nums[0], nums[1])
+        if len(nums) >= 2:
+            for i in range(2, len(nums)):
+                c = lcm(c, nums[i])
+    
+    return c
 
 def chebyshev_2lcm(x):
-  return math.log(LCM_rep(x), math.e)
+    return math.log(LCM_rep(x), math.e)
+
+
+def chebyshev_2(x):
+    """
+    primes = primegen(x)
+    summed = 0
+    for m in range(2, x+1):
+        if m in primes:
+            summed += math.floor(math.log(x, m)) * math.log(m, math.e)
+    return summed
+    """
+    summed = 0
+    for i in range(x):
+        summed += vonmangoldt(i+1)
+    return summed
+
+def epsi(x):
+    return math.exp(chebyshev_2(x))
+
+
+
+b = 0
+while True:
+    try:
+        m = epsi(b)
+    except:
+        m = "ERR"
+    print(str(b) + " - " + str(m))
+    b += 1
--- python/math/primegen.py
+++ python/math/primegen.py
@@ -13,10 +13,10 @@ def primecheck(x):
     
 def primegen(x):
   primes = []
-  for i in range(2, x):
+  for i in range(2, x+1):
     n = primecheck(i)
-    if i != 0:
-      primes.append(x)
+    if n != 0:
+      primes.append(i)
   return primes
 
 def pk(n):
@@ -24,8 +24,9 @@ def pk(n):
   for p in range(len(primes)):
     x = 0
     m = n
-    while m % primes[p] == 0:
+    while m >= 1:
       m = m/primes[p]
-    if m == 1:
-      return (primes[p], x)
+      x += 1
+    if m == 1/primes[p]:
+      return (primes[p], x-1)
   return (0,0)
